aabd10a1f653184a199eba675a4347f50d47af23
mrbootyman <mironovline@gmail.com>
2024-07-23 21:39:01 +0500

    Add files via upload

--- parser_santemo.js
+++ parser_santemo.js
@@ -1,19 +1,31 @@
 const { JSDOM } = require('jsdom');
+const fs = require('fs');
+//const names = [];
+//const times = [];
+//const links = [];
+//const imglinks = [];
 const data = [];
 
 (async () => {
     try {
         const dom = await JSDOM.fromURL('https://santemo.ru/sales/');
         const d = dom.window.document;
-        let blocks = d.querySelectorAll('div.dark_link.color-theme-target');
+        let blocks = d.querySelectorAll('div.sale-list__wrapper.grid-list__item');
         blocks.forEach( block => {
-            let name = block.querySelector('div.dark_link.color-theme-target').textContent;
-            //let time = block.querySelector('div.page-section__item-date').textContent;
-            data.push(name);
-            console.log(`название акции: ${name}`);
+            let name = block.querySelector('a.dark_link.color-theme-target').textContent;
+            let time = block.querySelector('span.countdown__active-to.hidden').textContent;
+            let link = "https://santemo.ru" + block.querySelector('a.sale-list__item-link').getAttribute('href');
+            let imglink = "https://santemo.ru" + block.querySelector('span.sale-list__item-image').getAttribute('data-bg');
+            data.push(name.trim(),time.trim(),link, imglink);
+            console.log(`название акции: ${name.trim()} ; сроки проведения: ${time.trim()}; ссылка: ${link}`);
         });
         if (data.length > 0) {
             console.log(JSON.stringify(data, null, ' '));
+            fs.writeFile('data.csv', JSON.stringify(data), (err) => {
+                if (err) throw err;
+        
+                console.log("saved");
+            });
         }
     } catch (e) {
         console.log(e);
